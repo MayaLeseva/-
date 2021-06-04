087874f918d8fb11ad28737bb86ab235a44d3086
MayaLeseva <82907638+MayaLeseva@users.noreply.github.com>
2021-06-04 12:43:58 +0300

    Update bikove_kravi.cpp

--- bikove_kravi.cpp
+++ bikove_kravi.cpp
@@ -7,6 +7,8 @@ int main()
     srand(time(0));
     int programa = rand() % 9000 + 1000, potrebitel;
     int programa1, programa2, programa3, programa4, potrebitel1, potrebitel2, potrebitel3, potrebitel4;
+    cout<<"Type your guess!";
+    cin>>potrebitel;
     programa1
     programa2
     programa3
