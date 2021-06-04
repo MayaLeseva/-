6378b4b87d99b612a93dbf4bc1914a9f8f97a887
MayaLeseva <82907638+MayaLeseva@users.noreply.github.com>
2021-06-04 12:50:06 +0300

    Update bikove_kravi.cpp

--- bikove_kravi.cpp
+++ bikove_kravi.cpp
@@ -9,13 +9,13 @@ int main()
     int programa1, programa2, programa3, programa4, potrebitel1, potrebitel2, potrebitel3, potrebitel4;
     cout<<"Type your guess!";
     cin>>potrebitel;
-    programa1
-    programa2
-    programa3
-    programa4
-    potrebitel1
-    potrebitel2
-    potrebitel3
-    potrebitel4
+    programa1=programa%10;
+    programa2=programa/10%10;
+    programa3=programa/100%10;
+    programa4=programa/1000;
+    potrebitel1=potrebitel%10;
+    potrebitel2=potrebitel/10%10;
+    potrebitel3=potrebitel/100%10;
+    potrebitel4=potrebitel/1000;
     return 0;
 }
