9de71f29fb98185e8497de6358a6ea3c654b8a9c
MayaLeseva <82907638+MayaLeseva@users.noreply.github.com>
2021-06-04 12:42:32 +0300

    .

--- bikove_kravi.cpp
+++ bikove_kravi.cpp
@@ -5,6 +5,15 @@ using namespace std;
 int main()
 {
     srand(time(0));
-    int programa = rand() % 9000 + 1000;
+    int programa = rand() % 9000 + 1000, potrebitel;
+    int programa1, programa2, programa3, programa4, potrebitel1, potrebitel2, potrebitel3, potrebitel4;
+    programa1
+    programa2
+    programa3
+    programa4
+    potrebitel1
+    potrebitel2
+    potrebitel3
+    potrebitel4
     return 0;
 }
